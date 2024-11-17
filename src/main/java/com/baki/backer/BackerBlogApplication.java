22f10f91915c877cca60b2cf08d7ab0dc6002d38
HuitaePark <qkrgmlxo3174@gmail.com>
2024-11-17 16:30:51 +0900

    프로젝트 생성

---
+++ src/main/java/com/baki/backer/BackerBlogApplication.java
@@ -0,0 +1,13 @@
+package com.baki.backer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackerBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackerBlogApplication.class, args);
+	}
+
+}
